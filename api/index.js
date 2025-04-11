556bf6861cf18e402cc9115bcc521bc530d03ba4
HusnvUcil <ucilucil797@gmail.com>
2025-04-11 22:34:01 +0700

    penambahan welcome to the root

--- api/index.js
+++ api/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const routes = require('./routes'); // Import routes dari routes.js
+const routes = require('./routes');  // Import routes yang sudah didefinisikan
 
 const app = express();
 
 // Middleware untuk parsing JSON
 app.use(express.json());
 
-// Gunakan routes
-app.use('/api', routes); // Semua route akan diawali dengan /api
+// Menangani permintaan ke root ('/')
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
 
-// // Jalankan server di lokal
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => console.log(`Server running on port ${port}`));
+// Gunakan routes untuk API
+app.use('/api', routes);  // Semua route API akan diawali dengan /api
+
+// Jalankan server di lokal
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 // Export app untuk Vercel
 module.exports = app;
