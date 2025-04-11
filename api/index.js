80a73b14697dac63dd3e9e0c9d066fd6a5f8ba13
HusnvUcil <ucilucil797@gmail.com>
2025-04-11 22:21:14 +0700

    modif -pengahpusan listen local

--- api/index.js
+++ api/index.js
@@ -9,9 +9,9 @@ app.use(express.json());
 // Gunakan routes
 app.use('/api', routes); // Semua route akan diawali dengan /api
 
-// Jalankan server di lokal
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// // Jalankan server di lokal
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`Server running on port ${port}`));
 
 // Export app untuk Vercel
 module.exports = app;
