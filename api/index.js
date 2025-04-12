2eab77b7571ec28a55eb8fd5f513e723fb355db5
HusnvUcil <ucilucil797@gmail.com>
2025-04-12 14:26:26 +0700

    penambahan untuk mengahndle cors *

--- api/index.js
+++ api/index.js
@@ -6,6 +6,14 @@ const app = express();
 // Middleware untuk parsing JSON
 app.use(express.json());
 
+// 🔥 Tambahkan ini untuk mengaktifkan CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // atau 'http://localhost:5173' untuk lebih aman
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Menangani permintaan ke root ('/')
 app.get('/', (req, res) => {
   res.send('Welcome to the root');
